97ebe9282fc132d82b0ff1276931d057f4d326a2
Zhangmingyang Su <60785877+Zhangmingyang-Su@users.noreply.github.com>
2020-05-20 11:59:05 -0400

    Update Extract Topic.py

--- Extract Topic.py
+++ Extract Topic.py
@@ -1,1 +1,56 @@
+from pyspark.ml.feature import StopWordsRemover
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+# Get insights of the users
+# find the top frequent words
+
+
+# find all cat and dogs owners
+df_all_owner = df_pets.select('word').union(pred_all.filter(F.col('prediction') == 1.0).select('word'))
+
+# customize stop_words 
+stopwords_custom = ['im', 'get', 'got', 'one', 'hes', 'shes', 'dog', 'dogs', 'cats', 'cat', 'kitty', 'much', 'really', 'love','like','dont','know','want','thin',\
+                    'see','also','never','go','ive']
+
+remover1 = StopWordsRemover(inputCol="raw", outputCol="filtered")
+core = remover1.getStopWords()
+core = core + stopwords_custom
+remover = StopWordsRemover(inputCol="word", outputCol="filtered",stopWords=core)
+df_all_owner = remover.transform(df_all_owner)
+
+wc = df_all_owner.select('filtered').rdd.flatMap(lambda a: a.filtered).countByValue()
+df_all_owner.show(5)
+
+
+#wcSorted = wc.sort(lambda a: a[1])
+wcSorted = sorted(wc.items(), key=lambda kv: kv[1],reverse = True)
+wcSorted
+
+# draw the picture
+text = " ".join([(k + " ")*v for k,v in wc.items()])
+wcloud = WordCloud(background_color="white", max_words=20000, collocations = False,
+               contour_width=3, contour_color='steelblue',max_font_size=40)
+
+# Generate a word cloud image
+wcloud.generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+fig,ax0=plt.subplots(nrows=1,figsize=(12,8))
+ax0.imshow(wcloud,interpolation='bilinear')
+
+ax0.axis("off")
+display(fig)
+
+
+#Get all creators whenever the users label is True(cat/dog owner)
+df_create = df_pets.select('creator_name').union(pred_all.filter(F.col('prediction') == 1.0).select('creator_name'))
+
+df_create.createOrReplaceTempView("create_table")
+
+#get count
+create_count = spark.sql("select distinct creator_name, count(*) as Number\
+                          from create_table \
+                          group by creator_name \
+                          order by Number DESC")
 
